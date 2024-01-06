8cb2088f0d9e9d0a0141c1ddcc996491c4855ccb
Daniel Norris <dnorris443@gmail.com>
2024-01-06 17:50:35 +0000

    chore(config): update for new settings

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'EnvyWare Ltd Documentation',
+  tagline: 'EnvyWare Ltd',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://www.envyware.co.uk/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
